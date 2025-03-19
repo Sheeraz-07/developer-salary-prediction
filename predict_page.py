d6a6392c0c7672eaf0bf680f3a414e671134d760
Sheeraz-07 <sheerazsameer07@gmail.com>
2025-03-19 23:52:19 +0500

    I am finally uploading this project to github after keeping it in my inventory for a long time

---
+++ predict_page.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+regressor_loaded = data["model"]
+le_country = data["le_country"]
+le_education = data["le_education"]
+
+def show_predict_page():
+    st.title("DEVELOPER SALARY PREDICTOR")
+    st.write("""### We need some information to predict salary""")
+
+    countries = (
+        "United States",
+        "India",
+        "United Kingdom",
+        "Germany",
+        "Canada",
+        "Brazil",
+        "France",
+        "Spain",
+        "Australia",
+        "Netherlands",
+        "Poland",
+        "Italy",
+        "Russian Federation",
+        "Sweden"
+    )
+    education = (
+        "Less than a Bachelors",
+        "Bachelor’s degree",  # Corrected apostrophe
+        "Master’s degree",    # Corrected apostrophe
+        "Post grad"
+    )
+    country = st.selectbox("Country", countries)
+    education = st.selectbox("Education", education)
+
+    experience = st.slider("Years of Experience", 0, 50, 3)
+    ok = st.button("Predict Salary")
+    if ok:
+        X = np.array([[country, education, experience]])
+        X[:, 0] = le_country.transform(X[:, 0])
+        X[:, 1] = le_education.transform(X[:, 1])
+        X = X.astype(float)
+
+        salary = regressor_loaded.predict(X)
+        st.subheader(f"The estimated Salary is: ${salary[0]:.2f}")
+
